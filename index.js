cb85ac9e0e2cf31445e32b3e7b7eb2fe603c9d16
Chirag Aggarwal <chirag.aggarwal@varpal.com>
2023-06-05 12:55:45 +0530

    version4

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 
 const app = express();
 
@@ -10,8 +12,6 @@ const boards = {
     outputs: [false, false, false, false, false, false, false, false, false, false, false, false]
   }
 };
-
-app.use(express.static('public'));
 
 // Endpoint to get the status of an output
 app.get('/status', (req, res) => {
