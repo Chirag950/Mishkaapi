4b5262314f8e9e737f0922f36fb9c1a15b601fbc
Chirag Aggarwal <chirag.aggarwal@varpal.com>
2023-06-10 16:17:45 +0530

    version4

--- index.js
+++ index.js
@@ -29,9 +29,6 @@ app.get('/status', (req, res) => {
 });
 
 // Endpoint to turn on an output
-// ...
-
-// Endpoint to turn on an output
 app.get('/on', (req, res) => {
   const { id, output } = req.query;
   const outputIndex = parseInt(output) - 1;
@@ -40,11 +37,6 @@ app.get('/on', (req, res) => {
   if (board && outputIndex >= 0 && outputIndex < board.outputs.length) {
     board.outputs[outputIndex] = true;
     res.send('Output turned on');
-
-    // Redirect to the main page after 1 second
-    setTimeout(() => {
-      res.redirect('/');
-    }, 1000);
   } else {
     res.status(404).send('Output not found');
   }
@@ -59,18 +51,10 @@ app.get('/off', (req, res) => {
   if (board && outputIndex >= 0 && outputIndex < board.outputs.length) {
     board.outputs[outputIndex] = false;
     res.send('Output turned off');
-
-    // Redirect to the main page after 1 second
-    setTimeout(() => {
-      res.redirect('/');
-    }, 1000);
   } else {
     res.status(404).send('Output not found');
   }
 });
-
-// ...
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
